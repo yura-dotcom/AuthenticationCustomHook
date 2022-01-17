5ce18f61cf65ce8cddd13543c72fd5d53aa69ec0
master <devgeorg222@gmail.com>
2022-01-17 15:12:02 +0200

    Updates

---
+++ precache-manifest.8f0497f64fc5c464d2e9c4f38ad77650.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e37e6d9b742e7de0e730bb5df0c407b1",
+    "url": "/AuthenticationCustomHook/index.html"
+  },
+  {
+    "revision": "abf3d501e9b67ef94bf4",
+    "url": "/AuthenticationCustomHook/static/css/main.a5c5dee6.chunk.css"
+  },
+  {
+    "revision": "dcd63aacf81053199156",
+    "url": "/AuthenticationCustomHook/static/js/2.9c76b9a0.chunk.js"
+  },
+  {
+    "revision": "fe4cdd745462666d252711c3f664a082",
+    "url": "/AuthenticationCustomHook/static/js/2.9c76b9a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abf3d501e9b67ef94bf4",
+    "url": "/AuthenticationCustomHook/static/js/main.302298bf.chunk.js"
+  },
+  {
+    "revision": "8a9dcbf849f50412cf04",
+    "url": "/AuthenticationCustomHook/static/js/runtime-main.326120ef.js"
+  },
+  {
+    "revision": "1d39767b543f36e4e0eb38e4ae788ef9",
+    "url": "/AuthenticationCustomHook/static/media/frodo.1d39767b.png"
+  },
+  {
+    "revision": "873079cc22aea148b02a6d47375197ab",
+    "url": "/AuthenticationCustomHook/static/media/gandalf.873079cc.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.eb450a76c13b60450730ccdaed570d1a.js"
+  "/AuthenticationCustomHook/precache-manifest.8f0497f64fc5c464d2e9c4f38ad77650.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/AuthenticationCustomHook/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
